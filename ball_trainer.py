0be89ce6555b46dc8b9f1dde0b91c21f6a96c310
Bethel Roger <bethelroger33@gmail.com>
2020-01-20 00:13:41 +0300

    - Enter model as cmd arg

--- ball_trainer.py
+++ ball_trainer.py
@@ -1,10 +1,24 @@
 import os
+import argparse
 
 from detectron2 import model_zoo
 from detectron2.engine import DefaultTrainer
 from detectron2.config import get_cfg
 
 from ball_loader import register_coco_instances
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Detectron2 filtered categories trainer from builtin models")
+    parser.add_argument("--config-name",
+                        default="COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml",
+                        help="model .yml file name in configs folder of detectron2",
+                        )
+    return parser
+
+
+args = get_parser().parse_args()
 
 # resister datasets filtered by category
 dataset_name = 'ball_train'
@@ -16,7 +30,7 @@ register_coco_instances(dataset_name, {}, json_file,
                         img_root, category_names=category_names)
 
 # setup model cfg
-model_yml = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+model_yml = args.config_name
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file(
     model_yml))
