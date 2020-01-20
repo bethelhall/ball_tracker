066429b00b61a549a5a23b1c7a0f3f494dba04e8
Bethel Roger <bethelroger33@gmail.com>
2020-01-20 16:47:17 +0300

    - fix video-input bug mass_inferer

--- mass_inferer.sh
+++ mass_inferer.sh
@@ -9,6 +9,8 @@ COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml
 COCO-InstanceSegmentation/mask_rcnn_R_101_C4_3x.yaml
 "
 
+videos=../data/selected_videos/*
+
 lrs=(0.00025 0.02)
 iters=(200 300 800 1000)
 
@@ -18,11 +20,14 @@ do
     do
         for m in $models
         do
-            echo "training ${m} with lr: ${l} & iter: ${i}.........."
-            echo "..........................."
-            no_yaml_m=$(cut -d '.' -f 1 <<< $m)   # remove .extenstion from string
-            w="../data/output/${no_yaml_m}_lr${l}_iter${i}/model_final.pth" # finetuned model weight path
-            python ball_inferer.py --config-name $m --output ../data/results/tunes/ --opts MODEL.WEIGHTS $w SOLVER.MAX_ITER $i SOLVER.BASE_LR $l
+            for v in $videos
+            do
+                echo "infering video ${v} with model:${m} with lr: ${l} & iter: ${i}.........."
+                echo "..........................."
+                no_yaml_m=$(cut -d '.' -f 1 <<< $m)   # remove .extenstion from string
+                w="../data/output/${no_yaml_m}_lr${l}_iter${i}/model_final.pth" # finetuned model weight path
+                python ball_inferer.py --config-name $m --video-input $v --output ../data/results/tunes/ --opts MODEL.WEIGHTS $w SOLVER.MAX_ITER $i SOLVER.BASE_LR $l
+            done
         done
     done
 done
